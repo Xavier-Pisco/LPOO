ebebfac8e6799c3339920a8673d6245dc1f8fb2f
Xavier-Pisco <x.pisco@gmail.com>
2020-06-12 12:33:06 +0100

    Did 2013-geometry exam

---
+++ quiz2013-geometry/src/main/java/Circulo.java
@@ -0,0 +1,28 @@
+public class Circulo implements Figura{
+    private Ponto centro;
+    private int raio;
+
+    public Circulo(Ponto centro, int raio) {
+        if (raio <= 0) throw new IllegalArgumentException();
+        this.centro = centro;
+        this.raio = raio;
+    }
+
+    public Ponto getCentro() {
+        return centro;
+    }
+
+    public int getRaio() {
+        return raio;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI*raio*raio;
+    }
+
+    @Override
+    public double getPerimetro() {
+        return 2*Math.PI*raio;
+    }
+}
--- quiz2013-geometry/src/main/java/Ponto.java
+++ quiz2013-geometry/src/main/java/Ponto.java
@@ -1,3 +1,43 @@
-public class Ponto {
-    
+import java.util.Objects;
+
+public class Ponto implements Comparable<Ponto> {
+    private int x;
+    private int y;
+
+    public Ponto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ponto)) return false;
+        Ponto ponto = (Ponto) o;
+        return x == ponto.x &&
+                y == ponto.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public int compareTo(Ponto ponto) {
+        return this.equals(ponto) ? 0 : 1;
+    }
 }
